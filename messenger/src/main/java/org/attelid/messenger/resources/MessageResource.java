9f0ce8bba732dccf8d64d44bf8b3f59acd95555f
Di Cal <diletta.cal@gmail.com>
2018-06-21 14:03:34 +0200

    Implemented delete and update API

--- messenger/src/main/java/org/attelid/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/attelid/messenger/resources/MessageResource.java
@@ -2,8 +2,11 @@ package org.attelid.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -28,10 +31,29 @@ public class MessageResource {
 		return messageService.getAllMessages();
 	}
 	
+	//adds a new message in messages/
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	public String addMessage() {
-		return "Test of POST API";
+	@Consumes(MediaType.APPLICATION_JSON) //parameter
+	@Produces(MediaType.APPLICATION_JSON) 
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
+	}
+	
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON) 
+	@Produces(MediaType.APPLICATION_JSON) 
+	public Message updateMessage(@PathParam(value = "messageId") long messageId, Message message) {
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON) 
+	public void deleteMessage(@PathParam("messageId") long id) {
+		messageService.removeMessage(id);
 	}
 	
 	@GET
