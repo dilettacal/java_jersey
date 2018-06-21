0e48f150c84993c808af52a4010fb67e2ced8ffe
Di Cal <diletta.cal@gmail.com>
2018-06-21 07:58:35 +0200

    Added handling for JSON and POST method

--- messenger/src/main/java/org/attelid/messenger/resources/MessageResource.java
+++ messenger/src/main/java/org/attelid/messenger/resources/MessageResource.java
@@ -3,6 +3,7 @@ package org.attelid.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -22,17 +23,24 @@ public class MessageResource {
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)	
+	@Produces(MediaType.APPLICATION_JSON)	
 	public List<Message> getMessages(){
 		return messageService.getAllMessages();
 	}
 	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public String addMessage() {
+		return "Test of POST API";
+	}
 	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_XML)	
+	@Produces(MediaType.APPLICATION_JSON)	
 	public Message getMessage(@PathParam(value = "messageId") long messageId) {
 			return messageService.getMessage(messageId);
 	}
+	
+
 
 }
